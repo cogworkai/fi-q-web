c42c18264e225ed7419d52a6d837c86db7245be9
Chris Kirby <kirbycm@gmail.com>
2025-10-02 15:08:13 -0400

    Bug fix.

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/fi-q-web">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/privacy" element={<PrivacyPolicy />} />
